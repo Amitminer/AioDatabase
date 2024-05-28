b3611f41505218e5e4fc56d90daef6c4f7ba3980
AmitxD <81667979+Amitminer@users.noreply.github.com>
2024-05-28 20:54:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-    
+
 setup(
     name="aiodatabase",
     version="1.0.0",
