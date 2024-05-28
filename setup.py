0517fdffd2d9caa75ebdf5f30a1140c372279225
AmitxD <81667979+Amitminer@users.noreply.github.com>
2024-05-28 20:53:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ Setup script for aiodatabase package.
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
 setup(
     name="aiodatabase",
-    version="0.1.7",
+    version="1.0.0",
+    license="MIT",
     install_requires=["PyYAML", "aiomysql", "aiosqlite"],
     packages=find_packages(),
     package_data={
@@ -14,6 +18,8 @@ setup(
     },
     include_package_data=True,
     description="AioDatabase is a simple database abstraction layer for SQLite and MySQL.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="AmitxD",
     url="https://github.com/Amitminer/AioDatabase",
     classifiers=[
